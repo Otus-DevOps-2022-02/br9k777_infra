0da45bc6f58e9630be67a79ce8744291c2455017
Maxim Pavlov <minized@gmail.com>
2022-04-17 17:17:19 +0300

    add  startup script - yc_metadata_from_file.sh

--- yc_compute_instance_create.sh
+++ yc_compute_instance_create.sh
@@ -8,5 +8,5 @@ yc compute instance create \
   --zone=ru-central1-a \
   --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1604-lts,size=10GB \
   --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4,nat-address=51.250.65.124,ipv4-address=10.128.0.33  \
-  --metadata serial-port-enable=1 \
+  --metadata serial-port-enable=0 \
   --ssh-key ~/.ssh/appuser.pub
